39121e2c2b089f8cac909e9582694a47c10970fd
unknown <cs.yuan777@gmail.com>
2018-03-27 12:50:08 +0800

    add

---
+++ cmake-build-debug/CMakeFiles/Training.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Training.dir/main.cpp.obj"
+  "Training.pdb"
+  "Training.exe"
+  "Training.exe.manifest"
+  "libTraining.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Training.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
